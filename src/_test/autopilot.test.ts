2bffdd6aed119342897691381c4db477c115fffa
Yoichiro Tanaka <yoichiro@eisbahn.jp>
2018-10-21 19:24:32 +0900

    Add "Autopilot" feature.

---
+++ src/_test/autopilot.test.ts
@@ -0,0 +1,81 @@
+import test from "ava"
+import {Autopilot} from "../autopilot"
+import * as sinon from "sinon"
+import {Conversation, ConversationResponse} from "../conversation"
+
+test("When the assistant immediately returns no response", async t => {
+    const conversation = new Conversation(
+        require(__dirname + "/../../src/_test/test-credentials.json"),
+    )
+    conversation.locale = "en-US"
+    const autopilot = new Autopilot(
+        conversation,
+        __dirname + "/../../src/_test/test-autopilot.yml",
+        "simple")
+    const mockConversation = sinon.mock(autopilot._conversation)
+    mockConversation
+        .expects("say")
+        .withArgs("Hello")
+        .callsFake((phrase: string) => {
+            return new Promise<ConversationResponse>((resolve, reject) => {
+                const conversationResponse: ConversationResponse = {
+                    expectUserResponse: false,
+                    displayText: [
+                        "Hello, Yoichiro!",
+                    ],
+                }
+                resolve(conversationResponse)
+            })
+        })
+    mockConversation
+        .expects("say")
+        .withArgs("How is the weather today?")
+        .callsFake((phrase: string) => {
+            return new Promise<ConversationResponse>((resolve, reject) => {
+                const conversationResponse: ConversationResponse = {
+                    expectUserResponse: false,
+                    displayText: [
+                        "Fine. It will be max 25 degree.",
+                    ],
+                }
+                resolve(conversationResponse)
+            })
+        })
+    mockConversation
+        .expects("say")
+        .withArgs("cancel")
+        .callsFake((phrase: string) => {
+            return new Promise<ConversationResponse>((resolve, reject) => {
+                const conversationResponse: ConversationResponse = {
+                    expectUserResponse: false,
+                    displayText: [],
+                }
+                resolve(conversationResponse)
+            })
+        })
+    const mockAutopilot = sinon.mock(autopilot)
+    mockAutopilot
+        .expects("_output")
+        .once()
+        .withArgs(["Autopilot mode started.", ""])
+    mockAutopilot
+        .expects("_output")
+        .once()
+        .withArgs(["> Hello"])
+    mockAutopilot
+        .expects("_output")
+        .once()
+        .withArgs(["", sinon.match.object, ""])
+    mockAutopilot
+        .expects("_output")
+        .once()
+        .withArgs(["> How is the weather today?"])
+    mockAutopilot
+        .expects("_output")
+        .once()
+        .withArgs(["", sinon.match.object, ""])
+    await autopilot.start()
+    mockAutopilot.verify()
+    mockConversation.verify()
+    t.pass()
+})
---
+++ src/autopilot.ts
@@ -0,0 +1,88 @@
+import {Conversation} from "./conversation"
+import * as fs from "fs"
+import * as yaml from "js-yaml"
+import {AbstractCommand} from "./abstract-command"
+
+interface DialogDefinition {
+    locale: string,
+    dialogs: {
+        phrase: string,
+    }[],
+}
+
+export class Autopilot extends AbstractCommand {
+
+    _input: any
+
+    constructor(conversation: Conversation, input: string, level: string) {
+        super(conversation, level)
+        this._input = input
+    }
+
+    start(): Promise<void> {
+        return new Promise<void>(async (resolve, reject) => {
+            this._output([
+                "Autopilot mode started.",
+                "",
+            ])
+            try {
+                const definition = await this._loadInputFile(this._input)
+                this._conversation.locale = definition.locale
+                await this._dialogue(definition, 0)
+            } catch(e) {
+                try {
+                    await this._send("cancel")
+                } catch(_) {
+                }
+                this._output([
+                    "",
+                    `Conversation aborted: ${e.toString()}`,
+                ])
+            }
+            resolve()
+        })
+    }
+
+    _dialogue(definition: DialogDefinition, index: number): Promise<void> {
+        return new Promise<void>(async (resolve, reject) => {
+            try {
+                if (index < definition.dialogs.length) {
+                    const phrase = definition.dialogs[index].phrase
+                    this._output([
+                        `> ${phrase}`,
+                    ])
+                    const response = await this._send(phrase)
+                    this._output([
+                        "",
+                        response,
+                        "",
+                    ])
+                    await this._dialogue(definition, index + 1)
+                } else {
+                    try {
+                        await this._send("cancel")
+                    } catch(_) {
+                    }
+                }
+                resolve()
+            } catch(e) {
+                reject(e)
+            }
+        })
+    }
+
+    async _loadInputFile(filePath: string): Promise<DialogDefinition> {
+        return new Promise<DialogDefinition>((resolve, reject) => {
+            fs.readFile(fs.realpathSync(filePath), {
+                encoding: "utf8",
+            }, (err, data) => {
+                if (err) {
+                    reject(err)
+                } else {
+                    resolve(yaml.safeLoad(data) as DialogDefinition)
+                }
+            })
+        })
+    }
+
+}
