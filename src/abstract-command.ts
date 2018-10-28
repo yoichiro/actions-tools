3ab4eeafe1a39c2b4fcd2c0639def9984b89b50b
Yoichiro Tanaka <yoichiro@eisbahn.jp>
2018-10-28 17:45:56 +0900

    Improve the output message text.

--- src/abstract-command.ts
+++ src/abstract-command.ts
@@ -2,6 +2,7 @@ import {Conversation, ConversationResponse} from "./conversation"
 import * as ora from "ora"
 import * as fs from "fs"
 import * as path from "path"
+import * as stringifyObject from "stringify-object"
 
 const opn = require("opn")
 const player = require("play-sound")()
@@ -22,56 +23,98 @@ export abstract class AbstractCommand {
         this._outputDirectory = output
     }
 
-    _output(messages: (string | ConversationResponse)[]): void {
-        messages.forEach((message: string | ConversationResponse) => {
-            if (message instanceof Object && this._level === "simple") {
-                const displayText = (message as ConversationResponse).displayText
-                if (displayText) {
-                    displayText.forEach((text: string) => {
-                        console.log(text)
-                    })
-                } else {
-                    console.log("(no response)")
-                }
+    _outputAsText(message: string | ConversationResponse): void {
+        if (message instanceof Object && this._level === "simple") {
+            const displayText = (message as ConversationResponse).displayText
+            if (displayText) {
+                displayText.forEach((text: string) => {
+                    console.log(text)
+                })
+            } else {
+                console.log("(no response)")
+            }
+        } else {
+            if (message instanceof Object) {
+                console.log(stringifyObject(message, {
+                    indent: "  ",
+                    singleQuotes: false,
+                    transform: (obj: any, prop, originalResult): string => {
+                        if (prop === "audioOut") {
+                            if (obj["audioOut"]) {
+                                const totalLength = obj.audioOut
+                                    .map((x: any) => {
+                                        return x.length
+                                    }).reduce((a: any, c: any) => {
+                                        return a + c
+                                    }, 0)
+                                return `(${totalLength} bytes)`
+                            } else {
+                                return "(empty)"
+                            }
+                        }
+                        if (prop === "data") {
+                            if (obj) {
+                                return `(${obj["data"].length} characters)`
+                            } else {
+                                return `(empty)`
+                            }
+                        }
+                        return originalResult
+                    },
+                }))
             } else {
                 console.log(message)
             }
-            if (this._screen !== "off") {
-                const screenOut = (message as ConversationResponse).screenOut
-                if (screenOut) {
-                    if (this._screen === "file" || this._screen === "play") {
-                        const filename = path.join(this._outputDirectory, `actions-tools-${Date.now()}.html`)
-                        fs.writeFileSync(filename, screenOut.data)
-                        console.log(`(The HTML file of the screen_out.data created. ${filename})`)
-                        if (this._screen === "play") {
-                            opn(filename)
-                        }
+        }
+    }
+
+    _outputScreen(message: string | ConversationResponse): void {
+        if (message instanceof Object && this._screen !== "off") {
+            const screenOut = (message as ConversationResponse).screenOut
+            if (screenOut) {
+                if (this._screen === "file" || this._screen === "play") {
+                    const filename = path.join(this._outputDirectory, `actions-tools-${Date.now()}.html`)
+                    fs.writeFileSync(filename, screenOut.data)
+                    console.log(`(The HTML file of the screen_out.data created. ${filename})`)
+                    if (this._screen === "play") {
+                        opn(filename)
                     }
                 }
             }
-            if (this._audio !== "off") {
-                const audioOut = (message as ConversationResponse).audioOut
-                if (audioOut) {
-                    if (this._audio === "file" || this._audio === "play") {
-                        const filename = path.join(this._outputDirectory, `actions-tools-${Date.now()}.mp3`)
-                        audioOut.forEach((audioData, i) => {
-                            if (i === 0) {
-                                fs.writeFileSync(filename, audioData)
-                            } else {
-                                fs.appendFileSync(filename, audioData)
+        }
+    }
+
+    _outputAudio(message: string | ConversationResponse): void {
+        if (message instanceof Object && this._audio !== "off") {
+            const audioOut = (message as ConversationResponse).audioOut
+            if (audioOut) {
+                if (this._audio === "file" || this._audio === "play") {
+                    const filename = path.join(this._outputDirectory, `actions-tools-${Date.now()}.mp3`)
+                    audioOut.forEach((audioData, i) => {
+                        if (i === 0) {
+                            fs.writeFileSync(filename, audioData)
+                        } else {
+                            fs.appendFileSync(filename, audioData)
+                        }
+                    })
+                    console.log(`(The MP3 file of the audio_out.audio_data created. ${filename})`)
+                    if (this._audio === "play") {
+                        player.play(filename, (err: any) => {
+                            if (err) {
+                                console.log(err)
                             }
                         })
-                        console.log(`(The MP3 file of the audio_out.audio_data created. ${filename})`)
-                        if (this._audio === "play") {
-                            player.play(filename, (err: any) => {
-                                if (err) {
-                                    console.log(err)
-                                }
-                            })
-                        }
                     }
                 }
             }
+        }
+    }
+
+    _output(messages: (string | ConversationResponse)[]): void {
+        messages.forEach((message: string | ConversationResponse) => {
+            this._outputAsText(message)
+            this._outputScreen(message)
+            this._outputAudio(message)
         })
     }
 
